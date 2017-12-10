1a971e00421d33675f49bc6e0427c13193f7cff0
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-12-10 12:34:59 +0100

    Revert "Applying constraints"
    This reverts commit ec8b92b

---
+++ applying-constraints/src/main/java/org/agoncal/fascicle/beanvalidation/applyingconstraints/ex01/Order.java
@@ -0,0 +1,118 @@
+package org.agoncal.fascicle.beanvalidation.applyingconstraints.ex01;
+
+import javax.validation.constraints.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+// tag::adocSnippet[]
+public class Order {
+
+  @NotNull
+  @Pattern(regexp = "[C,D,M][A-Z][0-9]*")
+  private String orderId;
+  @NotNull
+  @Min(1)
+  private Double totalAmount;
+  @PastOrPresent
+  private Date creationDate;
+  private Date paymentDate;
+  @Future
+  private Date deliveryDate;
+  @Email
+  private String contactEmail;
+
+  @NotNull
+  private List<OrderLine> orderLines;
+
+  public Order(@Past Date creationDate) {
+    this.creationDate = creationDate;
+  }
+
+  @NotNull
+  public Double calculateTotalAmount(@Positive Double changeRate) {
+    return complexCalculation();
+  }
+
+  // tag::adocSkip[]
+  public Order() {
+  }
+
+  private Double complexCalculation() {
+    return 1d;
+  }
+
+  // ======================================
+  // =          Getters & Setters         =
+  // ======================================
+
+  public String getOrderId() {
+    return orderId;
+  }
+
+  public void setOrderId(String orderId) {
+    this.orderId = orderId;
+  }
+
+  public Date getCreationDate() {
+    return creationDate;
+  }
+
+  public void setCreationDate(Date creationDate) {
+    this.creationDate = creationDate;
+  }
+
+  public Double getTotalAmount() {
+    return totalAmount;
+  }
+
+  public void setTotalAmount(Double totalAmount) {
+    this.totalAmount = totalAmount;
+  }
+
+  public Date getPaymentDate() {
+    return paymentDate;
+  }
+
+  public void setPaymentDate(Date paymentDate) {
+    this.paymentDate = paymentDate;
+  }
+
+  public Date getDeliveryDate() {
+    return deliveryDate;
+  }
+
+  public void setDeliveryDate(Date deliveryDate) {
+    this.deliveryDate = deliveryDate;
+  }
+
+  public String getContactEmail() {
+    return contactEmail;
+  }
+
+  public void setContactEmail(String contactEmail) {
+    this.contactEmail = contactEmail;
+  }
+
+  public List<OrderLine> getOrderLines() {
+    return orderLines;
+  }
+
+  public void setOrderLines(List<OrderLine> orderLines) {
+    this.orderLines = orderLines;
+  }
+
+  public void addOrderLine(OrderLine orderLine) {
+    if (this.orderLines == null)
+      this.orderLines = new ArrayList<>();
+
+    this.orderLines.add(orderLine);
+  }
+  // end::adocSkip[]
+}
+// end::adocSnippet[]
---
+++ applying-constraints/src/test/java/org/agoncal/fascicle/beanvalidation/applyingconstraints/ex01/OrderTest.java
@@ -0,0 +1,112 @@
+package org.agoncal.fascicle.beanvalidation.applyingconstraints.ex01;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+public class OrderTest {
+
+  // ======================================
+  // =             Attributes             =
+  // ======================================
+
+  protected static ValidatorFactory vf;
+  protected static Validator validator;
+
+  private static Date creationDate;
+  private static Date paymentDate;
+  private static Date deliveryDate;
+
+
+  // ======================================
+  // =          Lifecycle Methods         =
+  // ======================================
+
+  @BeforeAll
+  public static void init() throws ParseException {
+    vf = Validation.buildDefaultValidatorFactory();
+    validator = vf.getValidator();
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+    creationDate = dateFormat.parse("01/01/2010");
+    paymentDate = dateFormat.parse("02/01/2010");
+    deliveryDate = dateFormat.parse("03/01/2045");
+
+  }
+
+  @AfterAll
+  public static void close() {
+    vf.close();
+  }
+
+  // ======================================
+  // =              Methods               =
+  // ======================================
+
+  @Test
+  public void shouldRaiseNoConstraintViolation() {
+
+    Order order = new Order(creationDate);
+    order.setOrderId("CA45678");
+    order.setTotalAmount(1234.5);
+    order.setDeliveryDate(deliveryDate);
+    order.setContactEmail("antonio.goncalves@gmail.com");
+    order.addOrderLine(new OrderLine("item", 12d, 2));
+
+    Set<ConstraintViolation<Order>> violations = validator.validate(order);
+    assertEquals(0, violations.size());
+  }
+
+  @Test
+  public void shouldRaiseViolationDueToEmail() {
+
+    Order order = new Order(creationDate);
+    order.setOrderId("CA45678");
+    order.setTotalAmount(1234.5);
+    order.setDeliveryDate(deliveryDate);
+    order.setContactEmail("dummy");
+    order.addOrderLine(new OrderLine("item", 12d, 2));
+
+    Set<ConstraintViolation<Order>> violations = validator.validate(order);
+    assertEquals(1, violations.size());
+  }
+
+  @Test
+  public void shouldRaiseViolationDueToEmptyLines() {
+
+    Order order = new Order(creationDate);
+    order.setOrderId("CA45678");
+    order.setTotalAmount(1234.5);
+    order.setDeliveryDate(deliveryDate);
+    order.setContactEmail("dummy");
+    order.addOrderLine(null);
+
+    Set<ConstraintViolation<Order>> violations = validator.validate(order);
+    assertEquals(1, violations.size());
+  }
+
+  private void displayContraintViolations(Set<ConstraintViolation<Order>> constraintViolations) {
+    for (ConstraintViolation constraintViolation : constraintViolations) {
+      System.out.println("### " + constraintViolation.getRootBeanClass().getSimpleName() +
+        "." + constraintViolation.getPropertyPath() + " - Invalid Value = " + constraintViolation.getInvalidValue() + " - Error Msg = " + constraintViolation.getMessage());
+
+    }
+  }
+}
