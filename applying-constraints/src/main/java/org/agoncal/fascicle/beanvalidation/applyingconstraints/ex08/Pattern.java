87ac2892e3c3e0120304ddded6d3ba937ed13e52
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-12-28 19:01:07 +0100

    Applying constraints

---
+++ applying-constraints/src/main/java/org/agoncal/fascicle/beanvalidation/applyingconstraints/ex08/Pattern.java
@@ -0,0 +1,29 @@
+package org.agoncal.fascicle.beanvalidation.applyingconstraints.ex08;
+
+import javax.validation.Constraint;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+// tag::adocSnippet[]
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+@Repeatable(Pattern.List.class)
+@Constraint(validatedBy = {})
+public @interface Pattern {
+
+  // ...
+
+  @Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+  @Retention(RUNTIME)
+  @Documented
+  @interface List {
+
+    Pattern[] value();
+  }
+}
+// end::adocSnippet[]
