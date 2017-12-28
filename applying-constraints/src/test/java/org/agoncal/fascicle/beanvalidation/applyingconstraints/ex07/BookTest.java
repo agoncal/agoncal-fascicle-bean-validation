904c4504f368f42942c3d6379d730310b85be29c
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-12-28 17:24:41 +0100

    Applying constraints

---
+++ applying-constraints/src/test/java/org/agoncal/fascicle/beanvalidation/applyingconstraints/ex07/BookTest.java
@@ -0,0 +1,129 @@
+package org.agoncal.fascicle.beanvalidation.applyingconstraints.ex07;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Antonio Goncalves
+ * <p>
+ * <p>
+ * http://www.antoniogoncalves.org
+ * --
+ */
+public class BookTest {
+
+  // ======================================
+  // =             Attributes             =
+  // ======================================
+
+  protected static ValidatorFactory vf;
+  protected static Validator validator;
+
+  // ======================================
+  // =          Lifecycle Methods         =
+  // ======================================
+
+  @BeforeAll
+  public static void init() {
+    vf = Validation.buildDefaultValidatorFactory();
+    validator = vf.getValidator();
+  }
+
+  @AfterAll
+  public static void close() {
+    vf.close();
+  }
+
+  // ======================================
+  // =              Methods               =
+  // ======================================
+
+  @Test
+  public void shouldRaiseNoConstraintViolation() {
+
+    Book book = new Book();
+    book.setTitle("title");
+    book.setNbOfPages(10);
+    book.setAuthorEmail("agoncal.fascicle@gmail.com");
+
+    Set<ConstraintViolation<Book>> violations = validator.validate(book);
+    assertEquals(0, violations.size());
+  }
+
+  @Test
+  public void shouldRaiseViolationDueToEmail() {
+
+    Book book = new Book();
+    book.setTitle("title");
+    book.setNbOfPages(10);
+    book.setAuthorEmail("dummy");
+
+    Set<ConstraintViolation<Book>> violations = validator.validate(book);
+    assertEquals(1, violations.size());
+  }
+
+  @Test
+  public void shouldRaiseNoConstraintViolationWithOptionalEmail() {
+
+    Book book = new Book();
+    book.setTitle("title");
+    book.setNbOfPages(10);
+    book.setAuthorEmail(null);
+
+    Set<ConstraintViolation<Book>> violations = validator.validate(book);
+    assertEquals(0, violations.size());
+  }
+
+  @Test
+  public void shouldRaiseViolationDueToNbPages() {
+
+    Book book = new Book();
+    book.setTitle("title");
+    book.setNbOfPages(-10);
+    book.setAuthorEmail("agoncal.fascicle@gmail.com");
+
+    Set<ConstraintViolation<Book>> violations = validator.validate(book);
+    assertEquals(1, violations.size());
+  }
+
+  @Test
+  public void shouldRaiseNoConstraintViolationWithOptionalNbPages() {
+
+    Book book = new Book();
+    book.setTitle("title");
+    book.setNbOfPages(null);
+    book.setAuthorEmail("agoncal.fascicle@gmail.com");
+
+    Set<ConstraintViolation<Book>> violations = validator.validate(book);
+    assertEquals(0, violations.size());
+  }
+
+  @Test
+  public void shouldRaiseNoConstraintViolationWithNullTitle() {
+
+    Book book = new Book();
+    book.setTitle(null);
+    book.setNbOfPages(10);
+    book.setAuthorEmail("agoncal.fascicle@gmail.com");
+
+    Set<ConstraintViolation<Book>> violations = validator.validate(book);
+    assertEquals(0, violations.size());
+  }
+
+  private void displayContraintViolations(Set<ConstraintViolation<Book>> constraintViolations) {
+    for (ConstraintViolation constraintViolation : constraintViolations) {
+      System.out.println("### " + constraintViolation.getRootBeanClass().getSimpleName() +
+        "." + constraintViolation.getPropertyPath() + " - Invalid Value = " + constraintViolation.getInvalidValue() + " - Error Msg = " + constraintViolation.getMessage());
+
+    }
+  }
+}
