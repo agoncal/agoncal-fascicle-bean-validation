0f4cfb2a454e5c3a4b30f5a9d58ed4cfef7509a9
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-12-31 16:36:36 +0100

    Fixed package name

--- writing-constraints/src/main/java/org/agoncal/fascicle/beanvalidation/writingconstraints/ex01/Size.java
+++ writing-constraints/src/main/java/org/agoncal/fascicle/beanvalidation/writingconstraints/ex01/Size.java
@@ -10,6 +10,7 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+// @formatter:off
 // tag::adocSnippet[]
 @Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
 @Retention(RUNTIME)
@@ -19,31 +20,16 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 public @interface Size {
 
   String message() default "{javax.validation.constraints.Size.message}";
-
   Class<?>[] groups() default {};
-
   Class<? extends Payload>[] payload() default {};
 
-  /**
-   * @return size the element must be higher or equal to
-   */
   int min() default 0;
-
-  /**
-   * @return size the element must be lower or equal to
-   */
   int max() default Integer.MAX_VALUE;
 
-  /**
-   * Defines several {@link javax.validation.constraints.Size} annotations on the same element.
-   *
-   * @see javax.validation.constraints.Size
-   */
   @Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
   @Retention(RUNTIME)
   @Documented
   @interface List {
-
     Size[] value();
   }
 }
