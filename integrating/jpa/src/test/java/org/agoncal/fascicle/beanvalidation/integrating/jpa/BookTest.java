4040b1a7aa87a787d9eda4df10d57fe097a2d7fd
Antonio Goncalves <antonio.goncalves@gmail.com>
2018-01-05 17:26:57 +0100

    Integrating JPA

---
+++ integrating/jpa/src/test/java/org/agoncal/fascicle/beanvalidation/integrating/jpa/BookTest.java
@@ -0,0 +1,55 @@
+package org.agoncal.fascicle.beanvalidation.integrating.jpa;
+
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+public class BookTest {
+
+  // ======================================
+  // =              Unit tests            =
+  // ======================================
+
+  @Test
+  public void shouldCreateAValidBook() throws Exception {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("beanvalidationPU");
+    EntityManager em = emf.createEntityManager();
+    EntityTransaction tx = em.getTransaction();
+    ;
+
+
+    // Creates an instance of book
+    Book book = new Book().title("H2G2").description("The Hitchhiker's Guide to the Galaxy").price(12.5F).isbn("1-84023-742-2").nbOfPages(354);
+
+    // Persists the book to the database
+    tx.begin();
+    em.persist(book);
+    tx.commit();
+    assertNotNull(book.getId(), "ID should not be null");
+
+    em.close();
+  }
+
+  @Test
+  public void shouldRaiseConstraintViolationCauseNullTitle() {
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("beanvalidationPU");
+    EntityManager em = emf.createEntityManager();
+    EntityTransaction tx = em.getTransaction();
+
+    Book book = new Book().title(null).description("The Hitchhiker's Guide to the Galaxy").price(12.5F).isbn("1-84023-742-2").nbOfPages(354);
+    assertThrows(RollbackException.class, () -> {
+      tx.begin();
+      em.persist(book);
+      tx.commit();
+    });
+  }
+}
